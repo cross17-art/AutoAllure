5fdf513a4ecfd48d0e73f9497033deb13a81811d
soovql <chloe.needles@gmail.com>
2023-06-13 20:53:10 +0300

    fixed footer, added media queries mixin and fixed minor errors

--- AutoAllure/src/component/screen/Footer.jsx
+++ AutoAllure/src/component/screen/Footer.jsx
@@ -1,52 +1,59 @@
-import { useState,useEffect } from 'react'
-import '../../assets/css/Information.scss'
-
+import { useState, useEffect } from "react";
+import "../../assets/css/Information.scss";
+import "../../assets/css/Footer.scss";
 
 function Footer() {
-
   return (
-       <>
-        <div className='information footer'>
-            
-            <div className='information_row'>
-
-                <div className='information_row-blockFooter'>
-                    <h1>
-                        We are always ready to help
-                    </h1>
-                    <p>
-                        You can always contact us if you need help with a car or housing in general.
-                        We are always happy to help you with the hotel, the organization of any events,
-                        excursions and generally fulfill any of your wishes.
-                        Our managers and support service are always happy to help.
-                    </p>
-                </div>
-                
-            </div>
-       
-            
+    <>
+      <div className="information footer">
+        <div className="information_row">
+          <div className="information_row-blockFooter">
+            <h1>We are always ready to help</h1>
+            <p>
+              You can always contact us if you need help with a car or housing
+              in general. We are always happy to help you with the hotel, the
+              organization of any events, excursions and generally fulfill any
+              of your wishes. Our managers and support service are always happy
+              to help.
+            </p>
+          </div>
         </div>
-        
+      </div>
 
-
-        <div className='information footerContact'>
-            
-         
-            <div className='information_row'>
-
-                <div className='information_row-blockFooterContact'>
-                    
-                    <p>Phone +357 9966 7777</p>
-                    <p>Email autoallure@protonmail.com</p>
-                    <p>Address
-Leoforos Amathous 32 Zanna Complex Block 1 Shop 8 Limassol 4532 </p>
-                </div>
-                
-            </div>
-            
+      <div className="information footerContact">
+        <div className="information_row">
+          <div className="footerContact_list">
+            <p className="footerContact_item">
+              Phone:{" "}
+              <a className="footerContact_item-link" href="tel: +35799667777">
+                {" "}
+                +357&nbsp;9966&&nbsp;7777
+              </a>
+            </p>
+            <p className="footerContact_item">
+              Email:{" "}
+              <a
+                className="footerContact_item-link"
+                href="mailto: autoallure@protonmail.com"
+              >
+                autoallure@protonmail.com
+              </a>
+            </p>
+            <p className="footerContact_item">
+              Address:{" "}
+              <a
+                className="footerContact_item-link"
+                href="https://goo.gl/maps/1zUCBWxVqK5apxuB7"
+                target="_blank"
+              >
+                Leoforos Amathous 32 Zanna Complex Block 1 Shop 8 Limassol 4532
+              </a>
+            </p>
+          </div>
         </div>
-        </>
-  )
+      </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
