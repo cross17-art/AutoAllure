841b7e6cc721f9a970e3ba15536d67c592fa64c3
cross17-art <cross_road02@mail.ru>
2023-06-19 01:54:01 +0300

    scss dropdonw

--- AutoAllure/src/component/ui/Dropdown.jsx
+++ AutoAllure/src/component/ui/Dropdown.jsx
@@ -4,42 +4,51 @@ import { useState } from "react";
 const Dropdown = ({ locationsDelivery }) => {
   const [isActive, setIsActive] = useState(false);
   const [selected, setIsSelected] = useState("Choose one ...");
-  function handleBlur(e) {
-    console.log(e);
-  }
+
+  const [isActiveTime, setIsTimeActive] = useState(false);
+  const [selectedTime, setIsTimeSelected] = useState("Choose one ...");
+
   return (
       <div className="dropdown">
-        <div
-          onClick={(e) => {
-            setIsActive(!isActive);
-          }}
-          className="dropdown-btn" name='dropdownText'
-        >
-          {selected}
+        <div className="dropdown_btn" name='dropdownText' onClick={(e) => {setIsActive(!isActive);}} >
+          {selected} 
           <span
             className={isActive ? "fas fa-caret-up" : "fas fa-caret-down"}
           />
+          <div className="dropdown_content" style={{ display: isActive ? "block" : "none" }}>
+            {locationsDelivery.map((element)=>{
+              return ( 
+                  <div onClick={(e) => {
+                    setIsSelected(e.target.textContent);
+                    setIsActive(!isActive);
+                  }}
+                  className="dropdown_content-item"
+                >
+                  {element.name}
+                </div>
+              )
+            })}
+          </div>
         </div>
-        <div
-          className="dropdown_content"
-          style={{ display: isActive ? "block" : "none" }}
-        >
-         {locationsDelivery.map((element)=>{
-           return ( 
-              <div
-              onClick={(e) => {
-                setIsSelected(e.target.textContent);
-                setIsActive(!isActive);
-              }}
-              className="dropdown_content-item"
-            >
-              {element.name}
-            </div>
-           )
-         })}
-
-         
-    
+        <div className="dropdown_btn" name='dropdownText' onClick={(e) => {setIsActive(!isActive);}} >
+          {selected} 
+          <span
+            className={isActive ? "fas fa-caret-up" : "fas fa-caret-down"}
+          />
+          <div className="dropdown_content" style={{ display: isActive ? "block" : "none" }}>
+            {locationsDelivery.map((element)=>{
+              return ( 
+                  <div onClick={(e) => {
+                    setIsSelected(e.target.textContent);
+                    setIsActive(!isActive);
+                  }}
+                  className="dropdown_content-item"
+                >
+                  {element.name}
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
   );
